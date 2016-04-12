b3bc71a30db23edebfc97aea69bac87c4897548b
Lucy Fox <lucyreneefox@gmail.com>
2016-04-12 14:23:44 -0600

    functioning pieces through iteration 2

--- lib/server.rb
+++ lib/server.rb
@@ -32,15 +32,25 @@ class Server
     verb = @request_lines[0].split[0]
     path = @request_lines[0].split[1]
     protocol = @request_lines[0].split[2]
-    request_hash = {"verb" => verb, "path" => path, "protocol" => protocol}
+    @request_hash = {"verb" => verb, "path" => path, "protocol" => protocol}
     @request_lines[1...@request_lines.length].each do |line|
       new_variable = line.split(': ')
-      request_hash[new_variable[0]] = new_variable[1]
+      @request_hash[new_variable[0]] = new_variable[1]
     end
+    @client.puts @request_hash
+    check_the_path
   end
 
-  def do_stuff
-
+  def check_the_path
+    if @request_hash["path"] == "/"
+      @client.puts @request_hash
+    elsif @request_hash["path"] == "/hello"
+      hello_world
+    elsif @request_hash["path"] == "/shutdown"
+      shutdown
+    elsif @request_hash["path"] == "/datetime"
+      datetime
+    end
   end
 
   def send_response
@@ -48,27 +58,21 @@ class Server
   end
 
   def hello_world
-    while (@client = @tcp_server.accept)
       output = "<html><head></head><body>#{"Hello World (#{@count_requests}) and this is our request #{@request_lines}"}</body></html>"
         @client.puts output
       @count_requests += 1
-      @client.close
-    end
   end
 
   def shutdown
-    @client = @tcp_server.accept
     output = "<html><head></head><body>#{"Total Requests: 12"}</body></html>"
     @client.puts output
     @tcp_server.close
   end
 
   def datetime
-    @client = @tcp_server.accept
-    time = Time.new
-    output = "<html><head></head><body>#{Time.new.hour} + #{":"} + #{Time.new.minute} + #{" on "} + #{Time.new.wday}</body></html>"
+    time = Time.new #probably want a whole separate format the time method
+    output = "<html><head></head><body>#{Time.new.ctime}</body></html>"
     @client.puts output
-    @client.close
   end
 
 # 11:07AM on Sunday, November 1, 2015.
