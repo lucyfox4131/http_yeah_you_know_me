c1d1416497281ef3c96c7192f54251731b97053c
Lucy Fox <lucyreneefox@gmail.com>
2016-04-11 22:10:35 -0600

    iteration 0 working in server class

--- lib/server.rb
+++ lib/server.rb
@@ -6,7 +6,22 @@ class Server
 
   def initialize
     @tcp_server = TCPServer.new(9292)
-    @client = @tcp_server.accept
+    @client #@tcp_server.accept
+    @count_requests = 1
+    # hello_world
+  end
+
+  def hello_world
+    while (@client = @tcp_server.accept)
+      request_lines = []
+      while line = @client.gets and !line.chomp.empty?
+        request_lines << line.chomp
+      end
+      output = "<html><head></head><body>#{"Hello World (#{@count_requests})"}</body></html>"
+        @client.puts output
+      @count_requests += 1
+      @client.close
+    end
   end
 
 
@@ -14,5 +29,5 @@ class Server
 end
 
 if __FILE__ == $PROGRAM_NAME
-  server = Server.new
+  server = Server.new.hello_world
 end
