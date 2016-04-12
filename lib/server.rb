e9f10d63eca06c336f8a8c345b8a6f634120e17b
Lucy Fox <lucyreneefox@gmail.com>
2016-04-12 14:07:00 -0600

    server start and parse request added

--- lib/server.rb
+++ lib/server.rb
@@ -6,28 +6,77 @@ class Server
 
   def initialize
     @tcp_server = TCPServer.new(9292)
-    @client #@tcp_server.accept
+    # @client #@tcp_server.accept
     @count_requests = 1
-    # hello_world
+    # @request_lines = []
+    start_server
+  end
+
+  def start_server
+    loop do
+      @request_lines = []
+      @client = @tcp_server.accept
+      get_request
+      parse_request
+      @client.close
+    end
+  end
+
+  def get_request
+    while line = @client.gets and !line.chomp.empty?
+      @request_lines << line.chomp
+    end
+  end
+
+  def parse_request
+    verb = @request_lines[0].split[0]
+    path = @request_lines[0].split[1]
+    protocol = @request_lines[0].split[2]
+    request_hash = {"verb" => verb, "path" => path, "protocol" => protocol}
+    @request_lines[1...@request_lines.length].each do |line|
+      new_variable = line.split(': ')
+      request_hash[new_variable[0]] = new_variable[1]
+    end
+  end
+
+  def do_stuff
+
+  end
+
+  def send_response
+
   end
 
   def hello_world
     while (@client = @tcp_server.accept)
-      request_lines = []
-      while line = @client.gets and !line.chomp.empty?
-        request_lines << line.chomp
-      end
-      output = "<html><head></head><body>#{"Hello World (#{@count_requests})"}</body></html>"
+      output = "<html><head></head><body>#{"Hello World (#{@count_requests}) and this is our request #{@request_lines}"}</body></html>"
         @client.puts output
       @count_requests += 1
       @client.close
     end
   end
 
+  def shutdown
+    @client = @tcp_server.accept
+    output = "<html><head></head><body>#{"Total Requests: 12"}</body></html>"
+    @client.puts output
+    @tcp_server.close
+  end
+
+  def datetime
+    @client = @tcp_server.accept
+    time = Time.new
+    output = "<html><head></head><body>#{Time.new.hour} + #{":"} + #{Time.new.minute} + #{" on "} + #{Time.new.wday}</body></html>"
+    @client.puts output
+    @client.close
+  end
+
+# 11:07AM on Sunday, November 1, 2015.
+# on #{Time.new.wday}, #{Time.new.month} #{Time.new.day}, #{Time.new.year}
 
 
 end
 
 if __FILE__ == $PROGRAM_NAME
-  server = Server.new.hello_world
+  server = Server.new
 end
